fb3a30c5b1daaaa84767724015d5229e9e6b0abd
Toan Tran <barneytran1998@gmail.com>
2020-12-03 17:33:42 -0600

    bouncing ball JS

---
+++ 2. JavaScript/bouncing balls/main.js
@@ -0,0 +1,111 @@
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  if (num === 0) return Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+Ball.prototype.update = function () {
+  if (this.x + this.size >= width) {
+    this.velX = -this.velX;
+  }
+
+  if (this.x - this.size <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if (this.y + this.size >= height) {
+    this.velY = -this.velY;
+  }
+
+  if (this.y - this.size <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10, 20);
+  let ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")",
+    size
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+  /*
+  requestAnimationFrame(loop):
+   Runs the function again - when this method is repeatedly run and passed the same function name, it runs that function a set number of times per second to create a smooth animation. This is generally done recursively - which means that the function is calling itself every time it runs, so it runs over and over again
+  */
+}
+
+Ball.prototype.collisionDetect = function () {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color =
+          "rgb(" +
+          random(0, 255) +
+          "," +
+          random(0, 255) +
+          "," +
+          random(0, 255) +
+          ")";
+
+        balls[j].velX = -balls[j].velX;
+        balls[j].velY = -balls[j].velY;
+      }
+    }
+  }
+};
+
+loop();
