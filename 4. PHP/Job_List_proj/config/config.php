4ad190da7cf48f38c575e2d5a62ed37269a4886f
Toan Tran <barneytran1998@gmail.com>
2020-12-12 14:57:36 -0600

    contact list React

--- 4. PHP/Job_List_proj/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// DB Params
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "joblister_db");
-
-define("SITE_TITLE", "JobLister");
--- 4. PHP/Job_List_proj/templates/inc/footer.php
+++
@@ -1,8 +0,0 @@
-  
-      <footer class="footer">
-        <p>&copy; 2016 JobLister.inc</p>
-      </footer>
-
-    </div> <!-- /container -->
- </body>
-</html>
