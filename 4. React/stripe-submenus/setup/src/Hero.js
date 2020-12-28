1b8475315213e06cf08ca4b643b0d2269adcf8c8
barneytran1998@gmail.com <barneytran1998@gmail.com>
2020-12-28 13:00:25 -0600

    cart submenu React

--- 4. React/stripe-submenus/setup/src/Hero.js
+++ 4. React/stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import {useGlobalContext} from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {closeSubmenu} = useGlobalContext();
+  
+  return <section className="hero" onMouseOver={closeSubmenu}>
+    <div className="hero-center">
+      <article className="hero-info">
+        <h1>Payments infrastructur for the internet</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsum tempore quas modi delectus minima commodi veniam quidem optio maiores!
+        </p>
+        <button className="btn">Start Now</button>
+      </article>
+      <article className="hero-images">
+        <img src={phoneImg} className='phone-img' alt="phone"/>
+      </article>
+    </div>
+  </section>
 }
 
 export default Hero
