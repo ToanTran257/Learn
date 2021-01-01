63fa2781ccd88f3442dfb4d7ccb5e3c51081ff18
barneytran1998@gmail.com <barneytran1998@gmail.com>
2021-01-01 15:59:16 -0600

    cocktails

--- 4. React/cocktails/setup/src/pages/About.js
+++ 4. React/cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, natus quo. Eum saepe inventore nostrum nulla repellat neque mollitia facilis nemo ducimus dolor reiciendis, quidem numquam libero quis possimus praesentium reprehenderit provident ad, voluptatum doloremque tenetur soluta maxime ipsum? Est velit consequatur debitis recusandae harum sit cum optio dolorem eligendi?
+      </p>
+    </section>
   )
 }
 
